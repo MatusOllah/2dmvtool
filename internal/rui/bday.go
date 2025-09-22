2a343d7905857da207d3ef1d3f7d3a7ee3306cee
Matúš Ollah <57183251+MatusOllah@users.noreply.github.com>
2025-09-22 09:46:03 +0200

    Remove Rui easter egg
    Remove Rui easter egg as I don't want to get in trouble with Colorful Palette for including an ANSI Rui in my code

--- internal/rui/bday.go
+++
@@ -1,9 +0,0 @@
-package rui
-
-import "time"
-
-// IsBirthday returns true if it's Rui's birthday (June 24rd) today.
-func IsBirthday() bool {
-	t := time.Now()
-	return t.Day() == 24 && t.Month() == time.June // June 24rd = Rui's birthday
-}
